f6a83ec4eda2dd813dbece8ea09ddd6384e53f17
Quentin Gliosca <quentin.gliosca@gmail.com>
2020-07-05 22:14:56 +0200

    Match change in ContentProtection interface

--- org.librarysimplified.r2.adobe/src/main/java/org/librarysimplified/r2/adobe/AcsContentProtection.kt
+++ org.librarysimplified.r2.adobe/src/main/java/org/librarysimplified/r2/adobe/AcsContentProtection.kt
@@ -5,7 +5,7 @@ import org.readium.r2.shared.fetcher.Fetcher
 import org.readium.r2.shared.fetcher.TransformingFetcher
 import org.readium.r2.shared.format.Format
 import org.readium.r2.shared.publication.ContentProtection
-import org.readium.r2.shared.publication.OnAskCredentialsCallback
+import org.readium.r2.shared.publication.OnAskCredentials
 import org.readium.r2.shared.publication.Publication
 import org.readium.r2.shared.util.File
 import org.readium.r2.shared.util.Try
@@ -21,7 +21,7 @@ class AcsContentProtection : ContentProtection {
     askCredentials: Boolean,
     credentials: String?,
     sender: Any?,
-    onAskCredentials: OnAskCredentialsCallback?
+    onAskCredentials: OnAskCredentials?
   ): Try<ContentProtection.ProtectedFile, Publication.OpeningError>? {
 
     if (file.format() != Format.EPUB)
